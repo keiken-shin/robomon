810a0ea8d346d2a4a2ad22bc08d33751ebdeb008
AnubhavK27 <37549611+AnubhavK27@users.noreply.github.com>
2020-04-08 15:37:58 +0530

    Updates

---
+++ precache-manifest.bada11fd5f1aa31bf05729c8de79fba4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7739f8a11afbfd565399cd892e1848c9",
+    "url": "/robomon/index.html"
+  },
+  {
+    "revision": "43bc75462f0b3e127d53",
+    "url": "/robomon/static/css/main.096afcfd.chunk.css"
+  },
+  {
+    "revision": "4a5f87f37665cf897075",
+    "url": "/robomon/static/js/2.abf38db2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robomon/static/js/2.abf38db2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43bc75462f0b3e127d53",
+    "url": "/robomon/static/js/main.8f161de5.chunk.js"
+  },
+  {
+    "revision": "f6f73861718d58d426c4",
+    "url": "/robomon/static/js/runtime-main.7e6623a2.js"
+  }
+]);
